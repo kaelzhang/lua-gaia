d7e6b357690ff2cce31d92d3d2b8b04aea3d74d6
Kael Zhang <i@kael.me>
2016-11-22 10:22:57 +0800

    installation for lua and cjson

---
+++ install/install.js
@@ -0,0 +1,22 @@
+require('shelljs/global')
+const path = require('path')
+
+function export_env (name, value) {
+  process.env['GAEA_' + name.toUpperCase()] = value
+}
+
+// const dir_sync = require('tmp').dirSync()
+// export_env('tmp', dir_sync.name)
+
+const platform = process.platform === 'darwin'
+  ? 'macosx'
+  : 'linux'
+export_env('lua_install_platform', platform)
+
+const PROJECT_ROOT = path.join(__dirname, '..')
+export_env('lua_cpath', path.join(PROJECT_ROOT, 'luac'))
+export_env('lua_path', path.join(PROJECT_ROOT, 'lua'))
+
+process.env.LUALIB = '~/lib/lua-5.3.3/src'
+
+exec(`bash ${PROJECT_ROOT}/install/install.sh`)
---
+++ install/install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# GAEA_TMP=${}
+#
+
+#
+# Exit with the given <msg ...>
+abort() {
+  printf "\n\x1B[31mError: $@\x1B[0m\n\n"
+  exit 1
+}
+
+log() {
+  local label=$1
+  shift
+  printf "\x1B[36m>>> %s\x1B[0m :" $label
+  printf " \x1B[90m$@\x1B[0m\n"
+}
+
+
+PREFIX=/usr/local
+DOWNLOAD_PREFIX=/usr/local/installer
+LUA_INCLUDE_DIR=${PREFIX}/include
+
+CJSON_SOURCE=$DOWNLOAD_PREFIX/installer
+GAEA_LUA_INSTALL_PLATFORM=${GAEA_LUA_INSTALL_PLATFORM-macosx}
+# GAEA_LUA_PATH
+# GAEA_LUA_CPATH
+
+
+mkdir $DOWNLOAD_PREFIX || echo "${DOWNLOAD_PREFIX} exists."
+
+
+clean_installer () {
+  log clean "old files"
+  rm -rf $DOWNLOAD_PREFIX/lua-5.5.3
+  rm $LUA_INCLUDE_DIR/lua
+  rm /usr/local/lib/liblua.dylib
+
+  rm -rf $DOWNLOAD_PREFIX/lua-cjson
+}
+
+
+install_lua () {
+  log download "lua source codes"
+  cd $DOWNLOAD_PREFIX
+  curl -O http://www.lua.org/ftp/lua-5.3.3.tar.gz
+  tar zxf lua-5.3.3.tar.gz
+  cd lua-5.3.3
+
+  log compile lua
+  # Build lua bin
+  make $GAEA_LUA_INSTALL_PLATFORM test
+
+  # link lua include
+  ln -sf $DOWNLOAD_PREFIX/lua-5.3.3/src $LUA_INCLUDE_DIR/lua
+
+  echo 'liblua.dylib: $(CORE_O) $(LIB_O)' >> src/makefile
+  echo -e '\t$(CC) -dynamiclib -o $@ $^ $(LIBS) -arch i386 -arch x86_64 -compatibility_version 5.3.3 -current_version 5.3.3 -install_name @rpath/$@' >> src/makefile
+
+  log compile liblua.dylib
+  make -C src liblua.dylib
+
+  ln -sf $DOWNLOAD_PREFIX/lua-5.3.3/src/liblua.dylib /usr/local/lib/liblua.dylib
+}
+
+
+# install_luajit () {
+
+# }
+
+
+install_cjson () {
+  log download cjson
+  git clone https://github.com/kaelzhang/lua-cjson.git $DOWNLOAD_PREFIX/lua-cjson
+  cd $DOWNLOAD_PREFIX/lua-cjson
+
+  log compile cjson
+  make
+  cp $DOWNLOAD_PREFIX/lua-cjson/cjson.so $GAEA_LUA_CPATH
+}
+
+
+clean_installer
+install_lua
+# install_luajit
+install_cjson
